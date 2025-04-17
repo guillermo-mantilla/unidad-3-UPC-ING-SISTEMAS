f9a3d2dea04450e7d07770c79afc83bacfacc852
guillermo-mantilla <guillermo-mantilla@upc.edu.co>
2025-04-16 21:57:04 -0500

    modificación en la rama

--- colegio.py
+++ colegio.py
@@ -6,4 +6,6 @@ print ("procede la segunda modificación")
 
 print ("realizo la cuarta modificación del archivo")
 
+# se crea la nueva rama y se cntimua con el trabajo colborativo
 
+# nueva modificación en la rama para integrar el código posteriormente
